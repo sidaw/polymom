1e4927f389996fd0e4287e5291fe7b67e53ddba8
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2014-05-01 18:32:22 -0700

    Added new packages and tests for broken quadratics

--- tests/polyopt_tests.py
+++ tests/polyopt_tests.py
@@ -23,6 +23,21 @@ def test_quadratic():
 
     assert np.allclose( x_opt, x_sol, 1e-3 )
 
+def test_cubic():
+    """
+    Test the simple quadratic (x-a)^2.
+    """
+    a = 2.
+
+    x = symbols('x')
+    pol = poly( (x - a)**6 )
+    x_opt = a
+
+    sol = polyopt.optimize_polynomial(pol)
+    x_sol = sol[x]
+
+    assert np.allclose( x_opt, x_sol, 1e-1 )
+
 def test_independent_quadratic():
     """
     x = 1, y = 2
@@ -57,4 +72,26 @@ def test_symmetric_quadratic():
     x_sol = sol[x], sol[y]
     # This doesn't work.
     assert np.allclose( [a, b], x_sol, 1e-3 ) == False
+
+def test_broken_quadratic():
+    """
+    x = 1, y = 2
+    Test the simple quadratic (pi * x + (1-pi) * y - 3)^2 + (x^2 + y^2 - 5)^2.
+    """
+
+    a, b, pi = 1., 2., 0.4,
+    x, y = symbols('x,y')
+    pol = polyopt.by_evaluation(
+            pi * x + (1-pi) * y,
+            pi * x**2 + (1-pi) * y**2,
+            x = a,
+            y = b)
+    sol = polyopt.optimize_polynomial(pol)
+    x_opt = np.array([a,b])
+    x_sol = sol[x], sol[y]
+
+    diff = np.sqrt((x_opt - x_sol).dot(x_opt - x_sol))
+    print diff
+    # This doesn't work.
+    assert np.allclose( x_opt, x_sol, 1e-1 ) 
 
