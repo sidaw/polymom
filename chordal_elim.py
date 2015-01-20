21d132d40f77a2db649fc76e19e053cd3ab5df0f
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2015-01-19 18:19:17 -0800

    Started implementation of clique eliminate.

--- chordal_elim.py
+++ chordal_elim.py
@@ -13,6 +13,47 @@ import numpy as np
 from sympy import symbols, poly, ring, QQ, lex, Symbol, groebner
 from util import nonzeros, first, prod
 
+class DAG(object):
+    def __init__(self):
+        self.vertices = set([])
+        self.outgoing_edges = {} 
+        self.incoming_edges = {} 
+
+    def __str__(self):
+        return "Graph(" + ", ".join(["%d -> %s"%(k, str(v)) for (k, v) in self.outgoing_edges.items()]) + ")"
+
+    def __repr__(self):
+        return "Graph: %d vertices, %d edges" % (len(self.vertices), len(self.outgoing_edges))
+
+    def add_vertex(self, u):
+        self.vertices.add(u)
+
+    def add_edge(self, u, v):
+        self.vertices.add(u)
+        self.vertices.add(v)
+        self.outgoing_edges[u] = self.outgoing_edges.get(u, [])
+        self.outgoing_edges[u].append(v)
+        self.incoming_edges[v] = self.outgoing_edges.get(v, [])
+        self.incoming_edges[v].append(u)
+
+    def get_parents(self, u):
+        return self.incoming_edges.get(u, [])
+
+    def get_children(self, u):
+        return self.outgoing_edges.get(u, [])
+
+    def get_clique(self, u):
+        return get_children(u) + [u]
+
+def ideal_to_graph(I):
+    g = DAG()
+    for f in I:
+        syms = sorted(list(active_symbols(f)))
+        for i in xrange(len(syms)):
+            for j in xrange(i+1, len(syms)):
+                g.add_edge(syms[i], syms[j])
+    return g
+
 # Running examples
 def example_simple():
     """
@@ -41,6 +82,34 @@ def example_order_preservation():
     R, x0, x1, x2, x3, x4 = ring('x0,x1,x2,x3,x4', QQ, lex)
     I = [x0 - x2, x0 - x3, x1 - x3, x1 - x4, x2 - x3, x3 - x4, x2**2]
     return R, I
+
+def example_subset_yes():
+    A = [1,2,3]
+    S = 5
+    n = len(A)
+    syms = [Symbol('s%d'%i) for i in xrange(n+1)]
+    R = ring(syms, QQ, lex)[0]
+    I = [syms[0]] + [(syms[i] - syms[i-1]) * (syms[i] - syms[i-1] - A[i-1]) for i in xrange(1,n+1)] + [syms[n] - S]
+    return R, as_ring_exprs(R, I)
+
+def example_subset_no():
+    A = [1,2,3]
+    S = 7
+    n = len(A)
+    syms = [Symbol('s%d'%i) for i in xrange(n+1)]
+    R = ring(syms, QQ, lex)[0]
+    I = [syms[0]] + [(syms[i] - syms[i-1]) * (syms[i] - syms[i-1] - A[i-1]) for i in xrange(1,n+1)] + [syms[n] - S]
+    return R, as_ring_exprs(R, I)
+
+def example_subset_large():
+    A = range(10)
+    S = 5
+    n = len(A)
+    syms = [Symbol('s%d'%i) for i in xrange(n+1)]
+    R = ring(syms, QQ, lex)[0]
+    I = [syms[0]] + [(syms[i] - syms[i-1]) * (syms[i] - syms[i-1] - A[i-1]) for i in xrange(1,n+1)] + [syms[n] - S]
+    return R, as_ring_exprs(R, I)
+
 
 #####
 
@@ -178,20 +247,90 @@ def eliminate(R, I, L=-1):
         J, K = split_gens(I, X_l)
         # Get lex grobner basis for J_l and append to J_l
         J = as_ring_exprs(R, groebner(as_polys(J), to_syms(R, X_l), domain=R.domain, order=R.order)) + J
-        # Eliminate x_l from J_l
-        print "J", J
-        J, _ = split_gens(set(J), X_l.difference([l]))
-        # TODO: eliminate x_l
-        #J = as_ring_exprs(R, groebner(as_polys(J), to_syms(R, X_l), domain=R.domain, order=R.order, wrt='x%d'%l))[1:]
-        print "J", J
-        # Construct W_l from the coeff ring.
-        W_ = [get_coefficient_term(R, f) for f in J] + K
-        if 1 in W_: W_ = 1
+        if 1 in J:
+            J = [R(1)]
+            W_ = []
+        else:
+            # Eliminate x_l from J_l: This is done by intersecting with K[X_l\x_l]
+            J, _ = split_gens(set(J), X_l.difference([l]))
+            print "J", J
+            # Construct W_l from the coeff ring.
+            W_ = [get_coefficient_term(R, f) for f in J] + K
+            if 1 in W_: W_ = 1
         # Re construct the next I
         I = J + K
         W.append(W_)
         print "I_l, W_l", I, W_
     return I, W
+
+def chord_eliminate(R, I):
+    """
+    Inputs:
+    $I$ - list[polynomial]: generators of ideal
+    $L$ - integer: number of elimination ideals to obtain
+    Output:
+    $[J_0, ..., J_{L-1}], [W_1, ..., W_L])$ - list[list[polynomial], list[polynomial]]: approximation of $elim_L(I)$
+    * Implicitly construct "chordal graph" G
+    """
+    syms = R.symbols
+
+    if L < 0:
+        L = len(syms) - 1
+
+    I = list(I) # copy
+    Js, Ws = []
+    for l in xrange(L):
+        # get clique X_l of "G"
+        X_l = get_clique(I, l)
+        print "X_l", X_l
+        # split the I_l in to two sets of generators J_l, K_l
+        J, K = split_gens(I, X_l)
+        # Get lex grobner basis for J_l and append to J_l
+        J = as_ring_exprs(R, groebner(as_polys(J), to_syms(R, X_l), domain=R.domain, order=R.order)) + J
+        if 1 in J:
+            J = [R(1)]
+            W = []
+        else:
+            # Eliminate x_l from J_l: This is done by intersecting with K[X_l\x_l]
+            J, _ = split_gens(set(J), X_l.difference([l]))
+            print "J", J
+            # Construct W_l from the coeff ring.
+            W = [get_coefficient_term(R, f) for f in J] + K
+            if 1 in W: W = [1]
+        Js.append(J)
+        Ws.append(W)
+        # Re construct the next I
+        I = J + K
+        print "I_l, W_l", I, W
+    return Js, Ws
+
+def clique_eliminate(R, I):
+    """
+    Inputs:
+    $I$ - list[polynomial]: generators of ideal
+    $L$ - integer: number of elimination ideals to obtain
+    Output:
+    $[J_0, ..., J_{L-1}], [W_1, ..., W_L])$ - list[list[polynomial], list[polynomial]]: approximation of $elim_L(I)$
+    * Implicitly construct "chordal graph" G
+    """
+
+    syms = R.symbols
+    n = len(syms)
+    G = ideal_to_graph(I)
+
+    Js, _ = chord_eliminate(R, I)
+    Hs = [None for _ in range(n)]
+    Hs[n-1] = Js[n-1]
+    for l in xrange(n-2,-1,-1):
+        # Get the largest child
+        p = max(G.get_children(l))
+        C = G.get_clique(p) + [l]
+        I = Hs[p] + Js[l]
+        order = MCS(G.get_clique(l))
+        R_ = R # with different ordering
+        Hs[l] = chord_eliminate(R, I)
+
+    return Hs
 
 def do_test(args):
     """
